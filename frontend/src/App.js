8d3aa89fdba2472b3e41a843163b08d6ebffec78
Louise Goco <gocolouise@gmail.com>
2026-03-30 00:26:58 +0800

    Edit profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <Register />;
+  return <Profile />;
 }
 
 export default App;
