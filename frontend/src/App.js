9bb1db52921cd185a6300b2094614580118c5ba0
NHMIER <darieflores@protonmail.com>
2026-03-31 12:11:34 +0800

    created a helelo world in aadminpanel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
+import AdminPanel from "./pages/AdminPanel";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <Login />;
+  return <AdminPanel />;
 }
 
 export default App;
