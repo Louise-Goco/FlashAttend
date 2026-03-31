7fe0363ad22a6e7c46945d43b7898b4b503215a0
NHMIER <darieflores@protonmail.com>
2026-03-31 11:54:25 +0800

    Coding login front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <Profile />;
+  return <Login />;
 }
 
 export default App;
